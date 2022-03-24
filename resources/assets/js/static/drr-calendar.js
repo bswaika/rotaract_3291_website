0c4261ecfc20d7ed80ce2d05c1ca120782703d6e
Baladitya Swaika <swaika@usc.edu>
2022-03-24 11:36:21 -0700

    Final codebase.

--- resources/assets/js/static/drr-calendar.js
+++ resources/assets/js/static/drr-calendar.js
@@ -3,7 +3,7 @@ $(function() {
     // page is now ready, initialize the calendar...
   
     $('#drr-calendar').fullCalendar({
-        googleCalendarApiKey: 'AIzaSyCe2H0Q2F5pm5iQwkoXiPRMK60lf8rvja0',
+        googleCalendarApiKey: 'some key',
         events: {
           googleCalendarId: 'soumya.drr3291@gmail.com'
         }
